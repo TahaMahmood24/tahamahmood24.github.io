ca637f1badc2a432e4eeaa7688ac428665c52ecf
Taha Mahmood <69377374+TahaMahmood24@users.noreply.github.com>
2023-09-15 14:53:34 -0700

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -46,14 +46,14 @@ const socialMediaLinks = {
 
 const skillsSection = {
   title: "What I do",
-  subTitle: "I am a Data Scientist who likes to explore",
+  subTitle: "Unravel the Mysteries of Data, One Algorithm at a Time 🕵️‍♂️",
   skills: [
     emoji(
-      "⚡ Develop highly interactive Front end / User Interfaces for your web and mobile applications"
+      "👨‍💻 Develop Machine Learning predictive model for forecasting, and other applications"
     ),
-    emoji("⚡ Progressive Web Applications ( PWA ) in normal and SPA Stacks"),
+    emoji("🖼️ Currently focusing on Machine Vision and Natural Language Processing"),
     emoji(
-      "⚡ Integration of third party services such as Firebase/ AWS / Digital Ocean"
+      "👷🏽‍♂️ Proficient in managing data infrastructure and data pipeline creation "
     )
   ],
 
@@ -62,56 +62,60 @@ https://fontawesome.com/icons?d=gallery */
 
   softwareSkills: [
     {
-      skillName: "html-5",
+      skillName: "Python",
+      fontAwesomeClassname: "fab fa-python"
+    },
+    {
+      skillName: "SQL",
+      fontAwesomeClassname: "fas fa-database"
+    },
+    {
+      skillName: "R",
+      fontAwesomeClassname: "fa-solid fa-r"
+    },
+    {
+      skillName: "HTML",
       fontAwesomeClassname: "fab fa-html5"
     },
     {
-      skillName: "css3",
+      skillName: "CSS",
       fontAwesomeClassname: "fab fa-css3-alt"
     },
     {
-      skillName: "sass",
-      fontAwesomeClassname: "fab fa-sass"
+      skillName: "Tableau",
+      fontAwesomeClassname: "fa-solid fa-chart-line"
     },
     {
       skillName: "JavaScript",
       fontAwesomeClassname: "fab fa-js"
     },
     {
-      skillName: "reactjs",
-      fontAwesomeClassname: "fab fa-react"
+      skillName: "MATLAB",
+      fontAwesomeClassname: "fa-solid fa-chart-simple"
     },
     {
-      skillName: "nodejs",
-      fontAwesomeClassname: "fab fa-node"
+      skillName: "PowerBI",
+      fontAwesomeClassname: "fa-solid fa-chart-pie"
     },
     {
-      skillName: "swift",
-      fontAwesomeClassname: "fab fa-swift"
+      skillName: "Git",
+      fontAwesomeClassname: "fa-brands fa-git"
     },
     {
       skillName: "npm",
       fontAwesomeClassname: "fab fa-npm"
     },
     {
-      skillName: "sql-database",
-      fontAwesomeClassname: "fas fa-database"
-    },
-    {
       skillName: "aws",
       fontAwesomeClassname: "fab fa-aws"
     },
     {
-      skillName: "firebase",
-      fontAwesomeClassname: "fas fa-fire"
+      skillName: "Azure",
+      fontAwesomeClassname: "fa-solid fa-a>"
     },
     {
-      skillName: "python",
-      fontAwesomeClassname: "fab fa-python"
-    },
-    {
-      skillName: "docker",
-      fontAwesomeClassname: "fab fa-docker"
+      skillName: "Google Cloud",
+      fontAwesomeClassname: "fa-brands fa-google"
     }
   ],
   display: true // Set false to hide this section, defaults to true
@@ -178,27 +182,26 @@ const workExperiences = {
     {
       role: "Regional Data Product Specialist",
       company: "Daraz (Alibaba Group)",
-      companylogo: require("./assets/images/Daraz_Logo.png"),
+      companylogo: require("./assets/images/Daraz-Icon-Vector.jpg"),
       date: "January 2023 – August 2023",
-      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
       descBullets: [
-        "Executed 7 Product A/B Testing strategies across the Buyer application such as Homepage, resulting in a 25% reduction in bounce rate and an increase in average user session duration from 1.35 minutes to 1.68 minutes",
-        "Engineered a robust product sentiment analysis solution, leveraging BERT-NLP techniques to extract insights from customer reviews with 86% accuracy, thereby improving strategic product curation process",
-        "Refined a product recommender system with a team of 5 engineers, revolutionizing application user experience through personalized product recommendations, increasing Product Click-Through rate by 12%",
-        "Led the Badge Project, a data-driven initiative to expand visibility for subsidized items through enhanced assortment display, boosting weekly Gross Merchandise Value by $0.95 million across all five ventures"
+        "Executed <b>7</b> Product A/B Testing strategies across the Buyer application such as Homepage, resulting in a <b>25%</b> reduction in bounce rate and an increase in average user session duration from <b>1.35</b> minutes to <b>1.68</b> minutes",
+        "Engineered a robust product sentiment analysis solution, leveraging BERT-NLP techniques to extract insights from customer reviews with <b>86%</b> accuracy, thereby improving strategic product curation process",
+        "Refined a product recommender system with a team of <b>5</b> engineers, revolutionizing application user experience through personalized product recommendations, increasing Product Click-Through rate by <b>12%</b>",
+        "Led the Badge Project, a data-driven initiative to expand visibility for subsidized items through enhanced assortment display, boosting weekly Gross Merchandise Value by <b>$0.95</b> million across all five ventures"
       ]
     },
     {
       role: "Management Trainee",
       company: "Daraz (Alibaba Group)",
-      companylogo: require("./assets/images/Daraz_Logo.png"),
+      companylogo: require("./assets/images/Daraz-Icon-Vector.jpg"),
       date: "July 2021 – December 2022",
-      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
       descBullets: [
-        "Executed 7 Product A/B Testing strategies across the Buyer application such as Homepage, resulting in a 25% reduction in bounce rate and an increase in average user session duration from 1.35 minutes to 1.68 minutes",
-        "Engineered a robust product sentiment analysis solution, leveraging BERT-NLP techniques to extract insights from customer reviews with 86% accuracy, thereby improving strategic product curation process",
-        "Refined a product recommender system with a team of 5 engineers, revolutionizing application user experience through personalized product recommendations, increasing Product Click-Through rate by 12%",
-        "Led the Badge Project, a data-driven initiative to expand visibility for subsidized items through enhanced assortment display, boosting weekly Gross Merchandise Value by $0.95 million across all five ventures"
+        "Refurbished the chatbot system by replacing static answers with AI-led adaptive answers, increasing the Chatbot Resolution Rate from <b>68%</b> to <b>79%</b>",
+        "Pioneered Live Commerce sequencing algorithm via IBCF-Feature Engineering to optimize stream ordering, Resulting in a significant increase from <b>105,000</b> to <b>126,000</b> Stream Daily Active Users (DAU)",
+        "Employed advanced time series forecasting techniques (Random Forest, XGBoost) to predict Live Commerce order surges, enabling precise demand planning and inventory management",
+        "Implemented an A/B Testing methodology via three distinct Machine Learning algorithms on assortment personalization, resulting in a <b>14%</b> improvement in Product Conversion",
+        "Collaborated with business leaders and stakeholders to establish the logic for <b>25</b> Live Commerce KPIs, subsequently developed a dashboard using SQL and Power BI to effectively monitor and visualize the metrics"
         ]
     }
   ]
